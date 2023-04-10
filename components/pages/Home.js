897f47255283e2b627897bb2fdf2a1e8cef74226
KZ211 <62577028+KZ211@users.noreply.github.com>
2023-04-10 20:27:11 -0300

    Empezando portfolio

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -6,10 +6,10 @@ import logo from "../../public/LogoAJ.jpg";
 function Home() {
   return (
     <section className={classes.Home}>
+      <div className={classes.div}>
       <h1>Home</h1>
-      <div>
         <img src="https://drive.google.com/uc?id=15BQm6J3z95VujzcFT4RDg73D3_2lgGYq" alt="Alexis Jares" width="700" />
-      <h2>Im frontend developer</h2>
+      <h2>Im frontend developer<br/> with react</h2>
       </div>
     </section>
   );
