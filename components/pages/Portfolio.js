a23e5b1564fb40430e07a31a50c59946416aa693
KZ211 <62577028+KZ211@users.noreply.github.com>
2023-04-10 21:28:41 -0300

    Grid casi completo

--- components/pages/Portfolio.js
+++ components/pages/Portfolio.js
@@ -4,14 +4,15 @@ import classes from "./Portfolio.module.css";
 
 function Portfolio() {
   return (
-    <section>
+    <section className={classes.section}>
       <div className={classes.title}>
       <h1>Portfolio</h1>
       </div>
       <div className={classes.gridContainer}>
-      <div className={classes.item1, classes.color1}>Hola</div>
-      <div className={classes.item2, classes.color2}>Hola</div>
-      <div className={classes.item3, classes.color3}>Hola</div>
+      <div className={classes.item1, classes.color1}>Hola1</div>
+      <div className={classes.item2, classes.color2}>Hola2</div>
+      <div className={classes.item3, classes.color3}>Hola3</div>
+      
       </div>
     </section>
   );
