c67a21415f892eb550052c811944ea08ef7f7a2f
KZ211 <62577028+KZ211@users.noreply.github.com>
2023-04-11 03:03:38 -0300

    Footer sin decoration y portfolio grid x6

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -7,20 +7,34 @@ function Footer() {
     <footer className={classes.footer}>
       <ul className={classes.ul}>
         <li className={classes.li}>
-          <a href="https://instagram.com" target="blank">
-            <img
-              src="https://img.icons8.com/ios-filled/256/instagram.png"
-              alt="GitHub"
-              width="60"
-            />
-            <h3>Instagram</h3>
-          </a>
+          <div>
+            <a href="https://www.instagram.com/alexis._.jares/" target="blank">
+              <img
+                src="https://img.icons8.com/ios-filled/256/instagram.png"
+                alt="Instagram"
+                width="60"
+              />
+              <h3>Instagram</h3>
+            </a>
+          </div>
         </li>
         <li className={classes.li}>
           <div>
-            <a href="https://instagram.com" target="blank">
+            <a href="https://www.facebook.com/alexis.jares.7/" target="blank">
               <img
-                src="https://img.icons8.com/windows/256/github.png"
+                src="https://img.icons8.com/ios-filled/256/facebook-new.png"
+                alt="Facebook"
+                width="60"
+              />
+              <h3>Facebook</h3>
+            </a>
+          </div>
+        </li>
+        <li className={classes.li}>
+          <div>
+            <a href="https://github.com/KZ211" target="blank">
+              <img
+                src="https://img.icons8.com/ios-filled/256/github.png"
                 alt="GitHub"
                 width="60"
               />
@@ -29,14 +43,19 @@ function Footer() {
           </div>
         </li>
         <li className={classes.li}>
-          <a href="https://linkedin.com" target="blank">
-            <img
-              src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/256/external-linkedin-social-media-tanah-basah-glyph-tanah-basah.png"
-              alt="GitHub"
-              width="60"
-            />
-            <h3>Linkedin</h3>
-          </a>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/alexis-jares-b70857235/"
+              target="blank"
+            >
+              <img
+                src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/256/external-linkedin-social-media-tanah-basah-glyph-tanah-basah.png"
+                alt="Linkedin"
+                width="60"
+              />
+              <h3>Linkedin</h3>
+            </a>
+          </div>
         </li>
       </ul>
       <p>Create by Alexis Jares | all rigth reseve | 10 de Abril de 2023 </p>
--- components/pages/Portfolio.js
+++ components/pages/Portfolio.js
@@ -21,6 +21,18 @@ function Portfolio() {
           <h2>Project 3</h2>
           <h1>Proximamente</h1>
         </div>
+        <div className={(classes.item4, classes.colorContainer)}>
+          <h2>Project 4</h2>
+          <h1>Proximamente</h1>
+        </div>
+        <div className={(classes.item5, classes.colorContainer)}>
+          <h2>Project 5</h2>
+          <h1>Proximamente</h1>
+        </div>
+        <div className={(classes.item6, classes.colorContainer)}>
+          <h2>Project 6</h2>
+          <h1>Proximamente</h1>
+        </div>
       </div>
     </section>
   );
