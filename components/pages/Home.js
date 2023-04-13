6bed93d426733c01099cfa366be73943e7503aac
KZ211 <62577028+KZ211@users.noreply.github.com>
2023-04-13 16:54:42 -0300

    100vh X page

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
       </nav>
       <div className={classes.div}>
       <h1>Home</h1>
-        <img src="https://drive.google.com/uc?id=1JG4hErb-UzCkC-hZnPjY0B5Lt9lOCHXN" alt="Alexis Jares" width="700" />
+        <img src="https://drive.google.com/uc?id=17ILR39_Rz_QvWJ1wF5nRFOKx4Afe4Aj-" alt="Alexis Jares" width="700" />
       <h2>I'm frontend developer<br/> with react</h2>
       </div>
     </section>
