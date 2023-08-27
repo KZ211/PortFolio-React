2681f7e6cbaac44ba52f0885edde9c592e45624d
KZ211 <62577028+KZ211@users.noreply.github.com>
2023-08-26 22:56:49 -0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import {Element} from "react-scroll";
+import React, { useState } from 'react';
+import { Element } from 'react-scroll';
 
 import Home from './components/pages/Home';
 import Portfolio from './components/pages/Portfolio';
 import AboutMe from './components/pages/AboutMe';
-import ContactMe from "./components/pages/ContactMe";
+import ContactMe from './components/pages/ContactMe';
 import Footer from './components/layout/Footer';
 import classes from './index.css';
 
-export default function App() {  
+export default function App() {
   return (
     <div className={classes.appContainer}>
       <Element name="home">
-          <Home />
-        </Element>
-        <Element name="portfolio">
-          <Portfolio />
-        </Element>
-        <Element name="aboutme">
-          <AboutMe />
-        </Element>
-        <Element name="contactme">
-          <ContactMe />
-        </Element>
-        <Footer />
+        <Home />
+      </Element>
+      <Element name="portfolio">
+        <Portfolio />
+      </Element>
+      <Element name="aboutme">
+        <AboutMe />
+      </Element>
+      <Element name="contactme">
+        <ContactMe />
+      </Element>
+      <Footer />
     </div>
   );
 }
